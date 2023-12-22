ff9b69f8958e2aee4a9a0ef74966ff6c0bc785d2
Scott Crowder <scrowder@gmail.com>
2023-12-22 12:58:18 -0600

    capture photo for person

--- FaceFacts/EditPersonView.swift
+++ FaceFacts/EditPersonView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct EditPersonView: View {
     
@@ -16,6 +17,8 @@ struct EditPersonView: View {
     
     @Binding var navigationPath: NavigationPath
     
+    @State private var selectedItem: PhotosPickerItem?
+    
     @Query(sort: [
         SortDescriptor(\Event.name),
         SortDescriptor(\Event.location)
@@ -23,6 +26,20 @@ struct EditPersonView: View {
     
     var body: some View {
         Form {
+            
+            Section {
+                
+                if let imageData = person.photo, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    Label("Select a photo", systemImage: "person")
+                }
+            }
+            
             Section {
                 TextField("Name", text: $person.name)
                     .textContentType(.name)
@@ -59,12 +76,19 @@ struct EditPersonView: View {
         .navigationDestination(for: Event.self) { event in
             EditEventView(event: event)
         }
+        .onChange(of: selectedItem, loadPhoto)
     }
     
     func addEvent() {
         let event: Event = Event(name: "", location: "")
         modelContext.insert(event)
         navigationPath.append(event)
+    }
+    
+    func loadPhoto() {
+        Task { @MainActor in
+            person.photo = try await selectedItem?.loadTransferable(type: Data.self)
+        }
     }
 }
 
