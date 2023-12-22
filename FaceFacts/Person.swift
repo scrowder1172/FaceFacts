e14572b5e36c1816a3d1fb6e75381d728df012fe
Scott Crowder <scrowder@gmail.com>
2023-12-22 11:12:37 -0600

    create person class

---
+++ FaceFacts/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  FaceFacts
+//
+//  Created by SCOTT CROWDER on 12/22/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Person {
+    var name: String
+    var emailAddress: String
+    var details: String
+    
+    init(name: String, emailAddress: String, details: String) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.details = details
+    }
+}
