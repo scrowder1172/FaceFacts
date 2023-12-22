d6049a3449ef3c4d9471d98891be78dc502b7895
Scott Crowder <scrowder@gmail.com>
2023-12-22 11:17:52 -0600

    initial UI with all data

--- FaceFacts/ContentView.swift
+++ FaceFacts/ContentView.swift
@@ -5,17 +5,27 @@
 //  Created by SCOTT CROWDER on 12/22/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Query var people: [Person]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(people) {person in
+                    NavigationLink(value: person) {
+                        Text(person.name)
+                    }
+                }
+            }
+            .navigationTitle("FaceFacts")
+            .navigationDestination(for: Person.self) { person in
+                Text(person.name)
+            }
         }
-        .padding()
     }
 }
 
