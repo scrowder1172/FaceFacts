e57d7b1eac2d383c2616d57c196e52e9f57131af
Scott Crowder <scrowder@gmail.com>
2023-12-22 11:04:36 -0600

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by SCOTT CROWDER on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
